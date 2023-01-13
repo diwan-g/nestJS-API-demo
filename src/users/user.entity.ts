e8d40fef17421145bc344cb35af14562643abb12
Gagan Diwan <azurepro@maas-MacBook-Pro.local>
2023-01-13 15:14:18 +0200

    added user entity CRUD to services

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,5 @@
 import {Entity, Column, PrimaryGeneratedColumn} from  'typeorm';
-
+import { AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
@@ -10,4 +10,19 @@ export class User {
 
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with id', this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User with id', this.id);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with id', this.id);
+    }
 }
