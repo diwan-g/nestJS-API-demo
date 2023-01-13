3511a7ecc20dd8d8cb402eff388abee09e184b41
Gagan Diwan <azurepro@maas-MacBook-Pro.local>
2023-01-13 15:53:51 +0200

    added routes/dtos to user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import {Body, Post, Controller } from '@nestjs/common';
+import {Body, Post, Controller, Get,Patch, Param,Query, Delete , NotFoundException} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
-
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -10,6 +10,29 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto ){
         this.userService.create(body.email, body.password)
+    }
 
+    @Get('/:id')
+    async findUser(@Param('id') id: string){
+        const user = await this.userService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string){
+        return this.userService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+        return this.userService.update(parseInt(id), body);
     }
 }
