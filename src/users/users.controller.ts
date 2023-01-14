a23090cb7ecf724020c3d69b0cc6c01297a343e6
Gagan Diwan <azurepro@maas-MacBook-Pro.local>
2023-01-14 09:03:06 +0200

    interceptor refactor 2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
-
+import { UserDto } from './dtos/user.dto';
 @Controller('auth')
 export class UsersController {
     constructor(private userService: UsersService){}
@@ -16,7 +16,7 @@ export class UsersController {
     createUser(@Body() body: CreateUserDto ){
         this.userService.create(body.email, body.password)
     }
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string){
         const user = await this.userService.findOne(parseInt(id));
